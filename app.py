8880578e9bb43bb328371cbec2309f8cbef20ee6
hairyElephant <josecalderon353@gmail.com>
2023-02-05 17:59:01 -0500

    added a lot entries

--- app.py
+++ app.py
@@ -17,9 +17,33 @@ db.connect()
 db.drop_tables([Show])
 db.create_tables([Show])
 
-Show(name= 'Naruto', main_character= 'Naruto Uzumaki', anime_type= 'Shonen').save()
+# Show(name= 'Naruto', main_character= 'Naruto Uzumaki', anime_type= 'Shonen').save()
+# Show(name= 'Attack On Titan', main_character= 'Eren Yeager', anime_type= 'Shonen').save()
 
-Show
+data = [
+    {"name": 'Dragonball Z', "main_character": 'Goku', "anime_type": 'Shonen'},
+    {"name": 'YuGhi-Oh', "main_character": 'Yugi', "anime_type": 'Shonen'},
+    {"name": 'Demon Slayer', "main_character": 'Tanjiro', "anime_type": 'Shonen'},
+    {"name": 'Rurouni Kenshin', "main_character": 'Kenshin Himura', "anime_type": 'Shonen'},
+    {"name": 'Naruto', "main_character": 'Naruto Uzumaki', "anime_type": 'Shonen'},
+    {"name": 'Attack On Titan', "main_character": 'Eren Yeager', "anime_type": "Shonen"},
+    {"name": "FullMetal Alchemist", "main_character": "Edward Elric", "anime_type": 'Shonen'},
+    {"name": 'Hellsing Ultimate', "main_character": 'Alucard', "anime_type": 'Seinen'},
+    {"name": 'Tokyo Ghoul', "main_character": 'Ken Kaneki', "anime_type": 'Seinen'},
+    {"name": 'One Punch Man', "main_character": 'Saitama', "anime_type": 'Seinen'},
+    {"name": 'Initial D First Stage', "main_character": 'Takumi Fujiwara', "anime_type": 'Seinen'},
+    {"name": 'Black Clover', "main_character": 'Asta', "anime_type": 'Shonen'},
+    {"name": 'Jujutsu Kaisen', "main_character": 'Yuji Itadori', "anime_type": 'Shonen'},
+    {"name": 'One Piece', "main_character": "Monkey D. Luffy", "anime_type": 'Shonen'},
+    {"name": 'Spy x Family', "main_character": 'Loid Forger (Twilight)', "anime_type": 'Shonen'},
+    {"name": 'Hunter x Hunter', "main_character": 'Gon Freecss', "anime_type": 'Shonen'},
+    {"name": 'Bleach', "main_character": 'Ichigo Kurosaki', "anime_type": 'Shonen'},
+    {"name": 'Fire Force', "main_character": 'Maki Oze', "anime_type": 'Shonen'},
+    {"name": 'YuYu Hakusho', "main_character": 'Yusuke Urameshi', "anime_type": 'Shonen'}
+
+]
+
+Show.insert_many(data).execute()
 
 app = Flask(__name__)
 
@@ -48,7 +72,7 @@ def endpoint(id=None):
         Show.delete().where(Show.id == id).execute()
         return "Show" + str(id) + "deleted."
 
-app.run()
+app.run(debug=True)
    
 
 # shonen: anime for young boys, shoujo: anime for young girls, seinen: anime for young adult males (18+), josei: anime for young adult females (18+), kodomomuke: anime for young children (4-12?)
