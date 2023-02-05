d593b1768caa2fee80d22561f28c8be2d09f7b02
hairyElephant <josecalderon353@gmail.com>
2023-02-05 13:37:01 -0500

    Added DB connection and Schema

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask, jsonify, request
 from peewee import *
 
-db = PostgresqlDatabase()
+db = PostgresqlDatabase('anime', user='postgres', password='12345', host='localhost', port=3000)
+
+class BaseModel(Model):
+    class Meta:
+        database = db 
+
+class Show(BaseModel):
+    name = CharField()
+    main_character = CharField()
+    anime_type = CharField()
+
+db.connect()
+db.drop_tables([Show])
+db.create_tables([Show])
+
+
+   
+
+# shonen: anime for young boys, shoujo: anime for young girls, seinen: anime for young adult males (18+), josei: anime for young adult females (18+), kodomomuke: anime for young children (4-12?)
